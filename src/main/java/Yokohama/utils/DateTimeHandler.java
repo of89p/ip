522eb6f7a4dc84fd7de1ed90da5d436a42123bb0
Terence Teo <terenceteoly@gmail.com>
2026-08-26 21:08:31 +0800

    docs: add javadoc for DateTimeHandler class

--- src/main/java/Yokohama/utils/DateTimeHandler.java
+++ src/main/java/Yokohama/utils/DateTimeHandler.java
@@ -3,8 +3,18 @@ package Yokohama.utils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Utility class for handling conversion between readable string datetime
+ * and LocalDatetime object for storage.
+ */
 public class DateTimeHandler {
-
+    /**
+     * Converts user entered deadline to LocalDateTime object.
+     * Throws exception when user-entered datetime is not of M/d/yyyy HHmm format.
+     *
+     * @param dateTime User entered deadline.
+     * @return LocalDateTime object to be stored in data file.
+     */
     public static LocalDateTime convertToLocalDateTime(String dateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy HHmm");
         try {
@@ -14,6 +24,12 @@ public class DateTimeHandler {
         }
     }
 
+    /**
+     * Converts LocalDateTime object back to readable String format.
+     *
+     * @param dateTime LocalDateTime of a Todo class.
+     * @return Readable string datetime.
+     */
     public static String formatToReadable (LocalDateTime dateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy HHmm");
         try {
